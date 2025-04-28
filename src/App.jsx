9af9d5b4bb092081af19e2471d4df84c84982fbf
mohan-16 <mohanrajg1630@gmail.com>
2025-04-28 14:17:45 +0530

    Mohan

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-import React from "react";
-import SignUp from "./pages/Auth/SignUp";
-=======
+
 import React from 'react'
 import LoginForm from './pages/Auth/LoginForm'
->>>>>>> 4be3c2ddc1fc9eec33ef09919c227b2ac24f3d37
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <SignUp />
-=======
       <LoginForm />
->>>>>>> 4be3c2ddc1fc9eec33ef09919c227b2ac24f3d37
     </>
   );
 }
 
-<<<<<<< HEAD
-export default App;
-=======
 export default App
->>>>>>> 4be3c2ddc1fc9eec33ef09919c227b2ac24f3d37
