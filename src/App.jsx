e6cdcfa48211a7349c11d9c5c2134025cf7e10fe
Murali-16 <murali16md@gmail.com>
2025-04-29 13:12:02 +0530

    murali

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LoginForm from './pages/Auth/LoginForm'
 import SignUp from './pages/Auth/SignUp';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import DataTable from './components/DataTable';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path='/login' element={<LoginForm/>}/>
       <Route path='/signup' element={<SignUp/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/DataTable' element={<DataTable/>}/>
      </Routes>
      </BrowserRouter>
     </>
--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Button,
+  Typography,
+  TextField,
+  Chip,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Stack,
+} from "@mui/material";
 
 const DataTable = () => {
   return (
-    <div>DataTable</div>
-  )
-}
+    <Box sx={{ p: 4, backgroundColor: "#f2f6f9", minHeight: "100vh" }}>
+      {/* <Box sx={{ mb: 2 }}>
+        <Typography variant="h6">Complaints ( 258 )</Typography>
+        <Typography variant="body2" color="text.secondary">
+          View list of Complaints Below
+        </Typography>
+      </Box> */}
 
-export default DataTable
+      <Box
+        sx={{
+          backgroundColor: "#fff",
+          borderRadius: 2,
+          p: 2,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextField
+          variant="outlined"
+          placeholder="Filter Your Search"
+          size="small"
+          sx={{ width: "40%" }}
+        />
+
+        {/* <Stack direction="row" spacing={1} alignItems="center">
+          <Chip label="Jan 12 - Mar 11" color="success" onDelete={() => {}} />
+          <Chip label="Solved" color="success" onDelete={() => {}} />
+          <Chip label="Himadri Boys Hostel" color="error" onDelete={() => {}} />
+          <Button variant="contained" color="primary">
+            + Add Complaint
+          </Button>
+        </Stack> */}
+        <Button variant="outlined"> + Add Complaint</Button>
+      </Box>
+
+      <TableContainer component={Paper} sx={{ mt: 3}}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell><b>Complaint Name</b></TableCell>
+              <TableCell><b>Complaint ID.</b></TableCell>
+              <TableCell><b>Nature of Complaint</b></TableCell>
+              <TableCell><b>Status</b></TableCell>
+              <TableCell><b>Type of Complaint</b></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* You can dynamically render rows here */}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default DataTable;
